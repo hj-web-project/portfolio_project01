91c485dd6aded3ed609f29b09f45b736a3d95c6c
hj-web-project <hjlee0714@gmail.com>
2026-09-21 17:15:08 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+</div> <!-- wrap end -->
+
+
+<?php if (basename($_SERVER['PHP_SELF']) === 'index.php') { ?>
+	
+<footer class="main-footer">
+	<div class="inner__cen1600 talign__center">
+		<p class="tx01">웹 퍼블리셔 / 웹 프론트엔드 개발자 이혜지의<br> 2026 포트폴리오</p>
+		<div class="ft-link-wr">
+			<a href="index.html" class="tx01">포트폴리오 홈</a>
+			<a href="javascript:void(0);" onclick="alert('준비 중입니다.');" class="tx01">실무 프로젝트 갤러리</a>
+			<a href="#main-contents-wr04" class="tx01">미팅신청</a>
+		</div>
+		<p class="tx01">© 2026 Leehaeji Portfolio</p>
+	</div>
+</footer>
+
+<?php } else { ?>
+
+<footer class="main-footer sub-footer">
+	<div class="inner__cen1600 talign__center">
+		<p class="tx01">웹 퍼블리셔 / 웹 프론트엔드 개발자 이혜지의<br> 2026 포트폴리오</p>
+		<div class="ft-link-wr">
+			<a href="../" class="tx01">포트폴리오 홈</a>
+			<a href="javascript:void(0);" onclick="alert('준비 중입니다.');" class="tx01">실무 프로젝트 갤러리</a>
+			<a href="../#main-contents-wr04" class="tx01">미팅신청</a>
+		</div>
+		<p class="tx01">© 2026 Leehaeji Portfolio</p>
+	</div>
+</footer>
+
+<?php } ?>
+
+
+
+</body>
+</html>
---
+++ project/project01.php
@@ -0,0 +1,75 @@
+<?php 
+  $page_title = "portfolio"; 
+  
+  include_once '../header.php'; 
+?>
+
+<div id="sub">
+	<div class="sub-title-wr">
+		<div class="inner__full">
+			<h1 class="title PlayfairDisplay">CAFE24 Store Build</h1>
+			<a href="https://leeddeok.co.kr/" target="_blank" class="view-btn PlayfairDisplay">View Website <span class="arrow">→</span></a>
+		</div>
+	</div>
+	
+	<section class="project-contents-wr">
+		<div class="project-inbox-wr">
+			<div class="tx-box-wr">
+				<div class="tx-box">
+					<h2 class="tit">Summary</h2>
+					<p class="tx01">Clients: 이낙근찹쌀떡</p>
+					<p class="tx01 tx02">Launching: 2025</p>
+					<ul>
+						<li>• Industry: Food</li>
+						<li>• Cafe24 Skin: Free</li>
+						<li>• Based In: KR</li>
+						<li>• Service Objective: 스토어 구축</li>
+					</ul>
+				</div>
+			</div>
+			<div class="img-box-wr img-wr"><img src="../images/lnk_detail_img01.jpg" alt="이낙근찹쌀떡"></div>
+		</div>
+		
+		<div class="project-inbox-wr">
+			<div class="img-box-wr img-wr"><img src="../images/lnk_detail_img03.jpg" alt="이낙근찹쌀떡"></div>
+			<div class="tx-box-wr">
+				<div class="tx-box">
+					<h2 class="tit">About</h2>
+					<p class="tx01">대한민국 제과 명장의 이름, 이낙근찹쌀떡
+51년 외길,<br><br> 오직 정직한 재료와 이낙근 장인의 손길로 빚어온 시간의 깊이입니다.
+한결같은 정성으로 지켜온 대한민국 대표 찹쌀떡의<br> 그 깊은 자부심을 당신께 전합니다
+					</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="solution-inbox-wr">
+			<div class="inner__cen768">
+				<h2 class="tit">The Solution</h2>
+				<p class="tx03">이낙근찹쌀떡이 지닌 대표성과 프리미엄 이미지를 강화하기 위해 브랜드 스토리를 시각적으로 정교하게 구성하고,<br> 화려하고 감각적인 인터랙션을 더해 브랜드의 깊은 가치를 보다 입체적이고 몰입감 있게 전달하도록 웹사이트를 설계했습니다.<br><br>
+				리뷰 앱 연동, 타임세일, AI 추천상품 기능, 카카오 알림톡의 마케팅 기능과<br> 1초 자동입금확인, 채널톡이 연동되어 운영되고 있습니다.</p>
+			</div>
+		</div>
+		
+		<div class="project-inbox-wr">
+			<div class="tx-box-wr">
+				<div class="tx-box">
+					<h2 class="tit">Want Similar<br> Results?</h2>
+					<a href="../index.html#main-contents-wr04" class="view-btn PlayfairDisplay button button--janus">
+						<svg class="button--janus-svg" viewBox="0 0 175 120" preserveAspectRatio="none" aria-hidden="true">
+							<path class="button--janus-fill"
+								fill="#000"
+								d="M154.5,88.5 C131,113.5 62.5,110 30,89.5 C-2.5,69 -3.5,42 4.5,25.5 C12.5,9 33.5,-6 85,3.5 C136.5,13 178,63.5 154.5,88.5 Z">
+							</path>
+						</svg>
+						<span>미팅신청</span>
+					</a>
+				</div>
+			</div>
+			<div class="img-box-wr img-wr"><img src="../images/lnk_img.jpg" alt="이낙근찹쌀떡"></div>
+		</div>
+	</section>
+</div>
+
+
+<?php include_once '../footer.php'; ?>
